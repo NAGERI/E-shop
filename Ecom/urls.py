4b961782f02447a4a48e4fb20fe6b0667a6c7f11
NAGERI <onacedricnageri@gmail.com>
2021-02-19 05:40:43 +0300

    first commit

---
+++ Ecom/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #Leave as empty string for base url
+	path('', views.Store, name='Store'),
+	path('cart/', views.Cart, name="Cart"),
+	path('checkout/', views.Checkout, name="Checkout"),
+
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+
+]
